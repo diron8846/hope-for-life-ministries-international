4ea3bfe92de328cf20e82b6d168ec7f859d44f30
Diron Mutuku <dironmutukurevival@gmail.com>
2026-05-11 06:41:55 +0000

    Deploy production build to gh-pages branch

--- src/components/IconBox.jsx
+++
@@ -1,8 +0,0 @@
-export const IconBox = ({ icon: Icon, children }) => (
-  <div className="flex items-center">
-    <div className="icon-3d mr-4">
-      <Icon />
-    </div>
-    <div>{children}</div>
-  </div>
-);
